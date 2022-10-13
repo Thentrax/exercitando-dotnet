3b97d08f1500fba2af66b779a6bbd182af11bf30
thiagoc <thiagoc1415@gmail.com>
2022-10-13 17:00:13 -0300

    feat: 5- Trabalhando com Json

---
+++ 5 - Trabalhando com Json/Program.cs
@@ -0,0 +1,10 @@
+﻿using _5___Nuget.Models;
+using Newtonsoft.Json;
+
+Venda produto = new Venda(0, "Mouse", 25.50M);
+
+// Serializar objetos em Json
+
+string serializado = JsonConvert.SerializeObject(produto, Formatting.Indented);
+
+Console.WriteLine(serializado);
