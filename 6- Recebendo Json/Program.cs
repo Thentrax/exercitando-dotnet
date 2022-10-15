2c1b168dd177f0745f8e354c7c609347bb08b76c
thentrax <thiagoc1415@gmail.com>
2022-10-15 09:34:07 -0300

    feat: 6- Rebendo arquivos Json

---
+++ 6- Recebendo Json/Program.cs
@@ -0,0 +1,11 @@
+﻿using _6__Recebendo_Json.Models;
+using Newtonsoft.Json;
+
+string conteudo = File.ReadAllText("Arquivos/vendas.json");
+
+List<Venda> vendas = JsonConvert.DeserializeObject<List<Venda>>(conteudo);
+
+foreach (Venda venda in vendas)
+{
+    Console.WriteLine($"Id:{venda.Id}, Nome: {venda.Produto}, Preço: R${venda.Preco}");
+}
