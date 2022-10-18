64c1c1f82f8d47f8e860392218617e36d0ad887d
thiagoc <thiagoc1415@gmail.com>
2022-10-18 15:04:52 -0300

    feat: 6- Hospedagem de Hotel

--- 5 - Trabalhando com Json/Program.cs
+++ 5 - Trabalhando com Json/Program.cs
@@ -1,10 +1,18 @@
 ﻿using _5___Nuget.Models;
 using Newtonsoft.Json;
 
-Venda produto = new Venda(0, "Mouse", 25.50M);
+List<Venda> vendas = new List<Venda>();
+
+Venda v1 = new Venda(0, "Mouse", 59.99M, DateTime.Now);
+Venda v2 = new Venda(1, "Teclado", 159.99M, DateTime.Now);
+
+vendas.Add(v1);
+vendas.Add(v2);
 
 // Serializar objetos em Json
 
-string serializado = JsonConvert.SerializeObject(produto, Formatting.Indented);
+string serializado = JsonConvert.SerializeObject(vendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json", serializado);
 
 Console.WriteLine(serializado);
